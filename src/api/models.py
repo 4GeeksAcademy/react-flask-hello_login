66673644ef6cf1f6f4424f999a5ac8b78015fbb0
Marcia0420 <138834592+Marcia0420@users.noreply.github.com>
2023-09-06 20:39:18 +0000

    modelado

--- src/api/models.py
+++ src/api/models.py
@@ -2,11 +2,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#     password = db.Column(db.String(80), unique=False, nullable=False)
+#     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+#     
 class User(db.Model):
+    # __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    lastname =db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+
+    favorite = db.relationship("Favorites")
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +28,78 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "lastname":self.lastname
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    # __tablename__ = 'people'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    hair_color =db.Column(db.String(250), nullable=False)
+    eye_color = db.Column(db.String(250), nullable=False)
+
+    favs = db.relationship("Favorites")
+
+    def __repr__(self):
+        return f'<People {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "hair_color":self.hair_color,
+            "eye_color":self.eye_color,
+            # do not serialize the password, its a security breach
+        }
+
+    # def to_dict(self):
+    #     return {}
+class Planets(db.Model):
+    # __tablename__ = 'planets'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250),nullable=False)
+    
+    fav = db.relationship("Favorites")
+
+
+    def __repr__(self):
+        return f'<Planets {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            
+        }
+
+    # def to_dict(self):
+
+    
+class Favorites(db.Model):
+    # __tablename__ = 'favorites'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    planets_id = db.Column(db.Integer,db.ForeignKey('planets.id'))
+
+
+    def __repr__(self):
+        return f'<Favorites {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_id":self.people_id,
+            "planets_id":self.planets_id,
+            # do not serialize the password, its a security breach
+        }
+    
